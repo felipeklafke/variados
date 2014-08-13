5b259eba09a084cd20beee14a54a90f67557dd2f
pasod <felipeklafke@live.com>
2014-08-12 22:44:54 -0300

    teste

---
+++ restaurar_backup_freebsd.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+
+"""
+Restaura o backup do banco de dados do gerenciador de pacotes PKG do freeBSD.
+"""
+
+import os
+import subprocess
+
+programas = subprocess.Popen("pkg info -ak | grep no", stdout=subprocess.PIPE, shell=True)
+(output, err) = programas.communicate()
+p_status = programas.wait()
+
+lista = output.split(" ")
+auxlista = []
+
+for a in lista:
+    a = a.strip()
+    if a!= "":
+        a = a.replace("no\n", "")
+        a = a.strip()
+        if len(a) > 3:
+            auxlista.append(a)
+
+
+for a in auxlista:
+    os.system("pkg delete -y {0}".format(a))
+    os.system("pkg install -y {0}".format(a))
