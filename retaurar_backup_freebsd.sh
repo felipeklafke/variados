a348eb23138eb879418b0032538071bdfe931fdf
felipe klafke <felipeklafke@live.com>
2015-02-04 03:58:39 -0200

    Delete retaurar_backup_freebsd.sh

--- retaurar_backup_freebsd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Desinstala e instala os programas que existem no banco de dados do PKG.
-
-programas=$(pkg info -x "^[a-z]" | grep -v pkg)
-pkg delete -y $programas
-pkg install -y $programas
